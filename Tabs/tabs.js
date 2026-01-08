d08cef9fe9793160a6ea6a9f894deebc5aa19d3b
Sriji <ksriji06@gmail.com>
2026-01-08 21:30:22 +0530

    p..

---
+++ Tabs/tabs.js
@@ -0,0 +1,21 @@
+//tabs configuration
+const tabsData = [
+  {
+    id: 1,
+    title: "Tab 1",
+    content: "This is a content for tab 1",
+  },
+
+  {
+    id: 2,
+    title: "Tab 2",
+    content: "This is a content for tab 2",
+  },
+  {
+    id: 3,
+    title: "Tab 3",
+    content: "This is a content for tab 3",
+  },
+
+  // we can add more tabs
+];
